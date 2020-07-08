5109372d40afebbbf2763f9b0156c54758d88c08
kennedywee <34017640+kennedywee@users.noreply.github.com>
2020-07-09 01:56:42 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,12 +11,14 @@ public class Main {
 		flights.add(new Flight("Kota Kinabalu", "Senai", "12-07-2020", "10:30 - 15:50"));
 		flights.add(new Flight("Senai", "Kota Kinabalu", "12-07-2020", "10:30 - 15:50"));
 		
+		
+		//Display
 		for(int i = 0; i < flights.size(); i++) {
 			int list = i+1;
-        	System.out.println("Flight Number: " + list);;
-            System.out.println("From: " + flights.get(i).getDestination1());
-            System.out.println("To: " + flights.get(i).getDestination2());
-            System.out.println("\n");
+        		System.out.println("Flight Number: " + list);;
+           		System.out.println("From: " + flights.get(i).getDestination1());
+            		System.out.println("To: " + flights.get(i).getDestination2());
+            		System.out.println("\n");
 		}
 
 	}
